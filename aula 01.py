dddf710a331964086ced553a99233e4531435ab9
DudaAcy <110108327+MariaEduardaAcyole@users.noreply.github.com>
2026-02-19 22:07:14 -0300

    Add files via upload

---
+++ aula 01.py
@@ -0,0 +1,14 @@
+import statistics as st
+lat_ms = [120, 130, 125, 128, 122, 600]
+
+media = st.mean(lat_ms)
+mediana = st.median(lat_ms)
+modas = st.multimode(lat_ms)
+dp_amostral = st.stdev(lat_ms) # amostral (n-1)
+var_amostral = dp_amostral ** 2 # variância amostral
+
+print("Média:", media)
+print("Mediana:", mediana)
+print("Moda(s):", modas)
+print("Variância amostral:", var_amostral)
+print("DP amostral:", dp_amostral)
