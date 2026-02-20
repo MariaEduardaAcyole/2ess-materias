df5741ed817d307952d8c45ca40d10339f2496ed
DudaAcy <110108327+MariaEduardaAcyole@users.noreply.github.com>
2026-02-19 22:22:42 -0300

    Enhance statistics calculations with Pandas
    Added statistics calculations using Pandas alongside existing statistics module calculations.

--- Estatistica/aula 01.py
+++ Estatistica/aula 01.py
@@ -1,14 +1,26 @@
 import statistics as st
 lat_ms = [120, 130, 125, 128, 122, 600]
 
+print("--- STATISTICS ---")
+
 media = st.mean(lat_ms)
-mediana = st.median(lat_ms)
 modas = st.mode(lat_ms)
+mediana = st.median(lat_ms)
 dp_amostral = st.stdev(lat_ms) # amostral (n-1)
 var_amostral = dp_amostral ** 2 # variância amostral
 
 print("Média:", media)
-print("Mediana:", mediana)
 print("Moda(s):", modas)
+print("Mediana:", mediana)
 print("Variância amostral:", var_amostral)
 print("DP amostral:", dp_amostral)
+
+print("--- PANDAS ---")
+
+import pandas as pd
+s = pd.Series([120, 130, 125, 128, 122, 600], name="lat_ms")
+print("Média:", s.mean())
+print("Moda:", s.mode().tolist())
+print("Mediana:", s.median())
+print("Variância amostral:", s.var(ddof=1))
+print("DP amostral:", s.std(ddof=1))
