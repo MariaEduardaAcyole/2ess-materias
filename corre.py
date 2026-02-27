e640a105f0dfe5df0f45d1f98b2f3af0f68dc083
MariaEduardaAcyole <dudaacyole12@gmail.com>
2026-02-27 19:32:54 -0300

    Primeiro commit

--- corre.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-x = [1,2,3,4,5,6,7,8,9,10]
-y = [2,4,5,7,8,9,11,12,14,15] # aumenta junto com x
-plt.scatter(x, y, color='blue')
-plt.title("Exemplo de correlação positiva")
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.show()
-
--- exe2-correlacao.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-x = [5,10,15,20,30,40,45,50,60,70]
-y = [50,80,100,120,150,160,180,200,210,220] # aumenta junto com x
-plt.scatter(x, y, color='blue')
-plt.title("Exercicio")
-plt.xlabel("min")
-plt.ylabel("valor")
-plt.show()
-
