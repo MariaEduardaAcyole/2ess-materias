0c1280608ac2f0b8c4de1aedef756c749603449c
DudaAcy <110108327+MariaEduardaAcyole@users.noreply.github.com>
2026-03-06 20:58:23 -0300

    Add files via upload

---
+++ Java/ex23.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ex23 {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int c,i,soma;
+        i=1;
+        for (c=1; c <= 100; c++) {
+                soma = c+1;
+            }
+        System.out.print("soma", soma);
+
+        }
+    }
